7e50a45ef0d74e42d9541915c66e2a5dfe2d4900
HyeseonBaek <bhs12087@naver.com>
2023-04-19 00:10:16 +0900

    [#3/feat] bottom navigationbar 클릭이벤트

--- app/src/main/java/org/android/go/sopt/presentation/MainActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/MainActivity.kt
@@ -4,15 +4,18 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.view.isInvisible
 import androidx.databinding.adapters.TextViewBindingAdapter.setText
+import androidx.fragment.app.Fragment
 import org.android.go.sopt.R
 import org.android.go.sopt.data.User
 import org.android.go.sopt.databinding.ActivityJoinBinding
 import org.android.go.sopt.databinding.ActivityMainBinding
+import org.android.go.sopt.presentation.home.GalleryFragment
 import org.android.go.sopt.presentation.home.HomeFragment
 import org.android.go.sopt.presentation.home.SearchFragment
-import org.android.go.sopt.utiil.UtilObject
 
 class MainActivity : AppCompatActivity() {
     private var userData : User? = null
@@ -22,11 +25,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
-        if(currentFragment == null){
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fcv_main,HomeFragment())
-                .commit()
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        binding.naviMain.setOnItemSelectedListener { item->
+            changeFragment(
+                when(item.itemId){
+                    R.id.menu_home->{
+                        HomeFragment()
+                    }
+                    R.id.menu_gallery->{
+                        GalleryFragment()
+                    }
+                    R.id.menu_search->{
+                        SearchFragment()
+                    }
+                    else->{
+                        throw IllegalArgumentException("can't find menu item id")
+                    }
+                }
+            )
+            true
         }
+        binding.naviMain.selectedItemId = R.id.menu_home
+    }
+
+    private fun changeFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fcv_main,fragment)
+            .commit()
     }
 }
