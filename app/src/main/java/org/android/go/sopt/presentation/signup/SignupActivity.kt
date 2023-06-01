a530b27a384e37a7928f57f2073e553835fd711c
HyeseonBaek <bhs12087@naver.com>
2023-06-01 22:44:35 +0900

    [#7/feat] 경고문구 구현, 정규식 추가

--- app/src/main/java/org/android/go/sopt/presentation/signup/SignupActivity.kt
+++ app/src/main/java/org/android/go/sopt/presentation/signup/SignupActivity.kt
@@ -4,23 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import android.content.Intent.EXTRA_USER
 import android.os.Bundle
-import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doOnTextChanged
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.android.go.sopt.R
 import org.android.go.sopt.data.model.AuthState
 import org.android.go.sopt.data.model.request.RequestSignUpDto
 import org.android.go.sopt.databinding.ActivitySignupBinding
 import org.android.go.sopt.presentation.login.LoginActivity
-import org.android.go.sopt.util.extension.hideKeyboard
 import org.android.go.sopt.util.extension.showToast
 
 @AndroidEntryPoint
@@ -33,33 +26,37 @@ class SignupActivity : AppCompatActivity() {
         binding = ActivitySignupBinding.inflate(layoutInflater)
         binding.vm = viewModel
         setContentView(binding.root)
-        setEditTextValidations()
         checkIsValidandFilled()
+        setErrorMsg()
         setSignUpBtnCheckEvent()
         checkSignUpSuccess()
-    }
-
-
-    private fun setEditTextValidations() {
-        with(binding){
-            signupEtId.doOnTextChanged { text, _, _, _ ->
-                viewModel.validateId(text.toString())
-            }
-            signupEtPw.doOnTextChanged { text, _, _, _ ->
-                viewModel.validatePassword(text.toString())
-            }
-            signupEtName.doOnTextChanged { text, _, _, _ ->
-                viewModel.validateName(text.toString())
-            }
-            signupEtSpecial.doOnTextChanged { text, _, _, _ ->
-                viewModel.validateSpeciality(text.toString())
-            }
-        }
     }
 
     private fun checkIsValidandFilled() {
         viewModel.isValidAndFilled.observe(this) { isValidAndFilled ->
             binding.signupBtn.isEnabled = isValidAndFilled
+        }
+    }
+
+    private fun setErrorMsg() {
+        viewModel.idErrorMessage.observe(this@SignupActivity) { errorMessageResId ->
+            binding.idInputLayout.error =
+                if (errorMessageResId != 0) getString(errorMessageResId) else null
+        }
+
+        viewModel.passwordErrorMessage.observe(this@SignupActivity) { errorMessageResId ->
+            binding.pwInputLayout.error =
+                if (errorMessageResId != 0) getString(errorMessageResId) else null
+        }
+
+        viewModel.nameErrorMessage.observe(this@SignupActivity) { errorMessageResId ->
+            binding.nameInputLayout.error =
+                if (errorMessageResId != 0) getString(errorMessageResId) else null
+        }
+
+        viewModel.specialityErrorMessage.observe(this@SignupActivity) { errorMessageResId ->
+            binding.specialityInputLayout.error =
+                if (errorMessageResId != 0) getString(errorMessageResId) else null
         }
     }
 
@@ -85,26 +82,26 @@ class SignupActivity : AppCompatActivity() {
         goToLogin()
     }
 
-    private fun checkSignUpSuccess(){
+    private fun checkSignUpSuccess() {
         viewModel.signupState.observe(this) { signupState ->
-            if(signupState == AuthState.SUCCESS){
+            if (signupState == AuthState.SUCCESS) {
                 goToLogin()
-            }else{
+            } else {
                 showToast(getString(R.string.join_fail_string))
             }
         }
     }
 
-    fun goToLogin() {
+    private fun goToLogin() {
         Intent(this, LoginActivity::class.java).apply {
             this.putExtra(EXTRA_USER, viewModel.getUser())
             setResult(Activity.RESULT_OK, this)
             if (!isFinishing) finish()
         }
     }
+
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-        val imm: InputMethodManager =
-            getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         return super.dispatchTouchEvent(ev)
     }
