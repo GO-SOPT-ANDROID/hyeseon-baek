3b1e57388ebcce5aaafd9a8c66af6fa7fbda0ee7
HyeseonBaek <bhs12087@naver.com>
2023-07-07 00:36:39 +0900

    [#9/chore] jvm 업그레이드

--- app/src/main/java/org/android/go/sopt/presentation/main/home/HomeViewModel.kt
+++ app/src/main/java/org/android/go/sopt/presentation/main/home/HomeViewModel.kt
@@ -1,20 +1,13 @@
 package org.android.go.sopt.presentation.main.home
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.android.go.sopt.R
 import org.android.go.sopt.data.model.AuthState
 import org.android.go.sopt.data.model.main.Follower
-import org.android.go.sopt.data.model.main.HomeItem
-import org.android.go.sopt.data.model.request.RequestSignInDto
-import org.android.go.sopt.data.model.response.ResponseFollowerDto
-import org.android.go.sopt.domain.AuthRepository
 import org.android.go.sopt.domain.FollowerRepository
-import retrofit2.Response
 import javax.inject.Inject
 
 @dagger.hilt.android.lifecycle.HiltViewModel
@@ -31,7 +24,7 @@ class HomeViewModel @Inject constructor(private val followerRepository: Follower
         getFollowerList()
     }
 
-    fun getFollowerList() {
+    private fun getFollowerList() {
         viewModelScope.launch {
             followerRepository.getUserList()
                 .onSuccess { response ->
